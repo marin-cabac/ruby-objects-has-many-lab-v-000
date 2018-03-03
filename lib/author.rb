51d804d038b33cf015fa5e9c3798d991b85094a6
Marin Cabac <marin.cabac@gmail.com>
2018-03-03 00:47:36 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,24 +1,30 @@
 class Author
   attr_accessor :name
   @@post_count
+
+###########################
   def initialize(n)
-    post_count+=1
+    post_count += 1
     @name=n
     @@posts_arr=[]
   end
+  ###########################
 
   def posts
     @@posts_arr
   end
+  ###########################
  def add_post(post)
    @@posts_arr << post
  end
+ ###########################
 def add_post_by_title(post_title)
   add_post(post_title)
   post_title.author = self
 end
+###########################
 def post_count
   @@post_count
 end
-
+###########################
 end
