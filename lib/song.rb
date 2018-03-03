a7a6c3b727907e4ff0bb2be4c45754f716097679
Marin Cabac <marin.cabac@gmail.com>
2018-03-03 09:30:38 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song
+  attr_accessor :name, :artist
+#############################################
+  def initialize(name, artist = nil)
+
+    @name = name
+    @artist = artist
+ end
+ #############################################
+ def artist_name
+   self.artist ? self.artist.name : nil
+#@artist.name !=nil  ? @artist.name : nil
+ end
+
+end
