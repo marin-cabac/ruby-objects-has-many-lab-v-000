29810115425205cf3cef076a7cd857992fcd34a0
Marin Cabac <marin.cabac@gmail.com>
2018-03-03 00:58:02 +0000

    automatically backed up by learn

---
+++ lib/post.rb
@@ -0,0 +1,16 @@
+class Post
+  attr_accessor :author
+  def initialize(title)
+    @title=title
+  end 
+  def title
+    @title
+  end 
+  def author
+    @author
+  end 
+  def author_name
+    self.author != nil ? @author.name : nil
+  end 
+end 
+    
